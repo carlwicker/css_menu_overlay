f9600ef827630fb3465bce8e78d0a8812476676f
Carl Wicker <carlwicker@gmail.com>
2019-06-05 18:49:53 +0100

    adding random background color to overlay menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,7 +9,12 @@ showMenu = false;
 
 function fadeInOutMenu() {
   if (!showMenu) {
+    randR = Math.floor(Math.random() * 255) + 1;
+    randG = Math.floor(Math.random() * 255) + 1;
+    randB = Math.floor(Math.random() * 255) + 1;
     overlayMenu.classList.add("show");
+    overlayMenu.style.backgroundColor =
+      "rgb(" + randR + ", " + randG + ", " + randB + ")";
     showMenu = true;
   } else {
     overlayMenu.classList.remove("show");
